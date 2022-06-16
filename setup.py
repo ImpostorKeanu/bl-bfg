08c96eb87294c50803a428c79e982dc44febade4
Justin Angel <justin@blackhillsinfosec.com>
2022-06-16 17:35:11 -0400

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='bl-bfg',
-    version='0.5.4.1',
+    version='0.5.4.2',
     author='Justin Angel',
     author_email='justin@arch4ngel.ninja',
     description='A simple password guessing framework.',
